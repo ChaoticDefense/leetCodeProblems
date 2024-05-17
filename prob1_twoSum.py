bc3a722763f81dc2461d2560d09dff1c3567dfc5
SuperSonicSeven <supersonicseven@gmail.com>
2024-05-17 09:24:03 -0500

    added prob1_twoSum

---
+++ prob1_twoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        
+        tmp1 = nums
+        tmp2 = target
+        
+        return nums
+    
+
+    print(twoSum([2,7,11,15], 9))
