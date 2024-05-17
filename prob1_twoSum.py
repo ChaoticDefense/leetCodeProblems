62bed4fce888310e50bfb9ac44117de15384cffa
ChaoticDefense <supersonicseven@gmail.com>
2024-05-17 18:16:17 -0500

    more work on prob 1

--- prob1_twoSum.py
+++ prob1_twoSum.py
@@ -1,11 +1,12 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         
-        tmp1 = nums
-        tmp2 = target
-        
-        nums.append(1)
+        for num1 in nums:
+            for num2 in nums:
+                if num1 != num2 and num1 + num2 == target:
+                    return [num1, num2]
 
-        return nums
+        return [num1, num2]
+
 s = Solution()
 print(s.twoSum([2,7,11,15], 9))
