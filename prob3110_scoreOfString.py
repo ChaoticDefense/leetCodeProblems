74d31303f8d153f5c0712fabe7bbcec87b8e52e6
ChaoticDefense <83382673+ChaoticDefense@users.noreply.github.com>
2024-06-02 07:44:47 -0500

    added comments to prob3110

--- prob3110_scoreOfString.py
+++ prob3110_scoreOfString.py
@@ -1,7 +1,10 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
+        # Initialize score value
         score = 0
 
+        # Iterate over string to the (n-1) character
+        # For each character, add up the absolute value difference of ASCII values
         for i in range(len(s) - 1):
             score += abs(ord(s[i]) - ord(s[i+1]))
 
