b89028df94b33f1df18be4516bca5eef91e6f59e
ChaoticDefense <supersonicseven@gmail.com>
2024-12-29 21:28:53 -0600

    improved max depth binary tree problem

--- algoMap/unit8_treesAndRecursion/prob104_maxDepthOfBinaryTree.py
+++ algoMap/unit8_treesAndRecursion/prob104_maxDepthOfBinaryTree.py
@@ -6,6 +6,8 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+    def __str__(self):
+        return str(self.val)
         
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
@@ -18,3 +20,18 @@ class Solution:
         
         # Return 1 plus the max depth between left and right
         return 1 + max(leftDepth, rightDepth)
+    
+    
+
+A = TreeNode(3)
+B = TreeNode(9)
+C = TreeNode(20)
+D = TreeNode(15)
+E = TreeNode(7)
+
+A.left = B
+A.right = C
+C.left = D
+C.right = E
+
+print(Solution().maxDepth(A))
