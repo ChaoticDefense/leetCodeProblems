5c749c99a67d9786986527ff2450d4d24dfab199
ChaoticDefense <supersonicseven@gmail.com>
2025-01-05 20:27:16 -0600

    last stone weight prob finished

--- algoMap/unit9_heapsAndPriorityQueues/prob1046_lastStoneWeight.py
+++ algoMap/unit9_heapsAndPriorityQueues/prob1046_lastStoneWeight.py
@@ -25,8 +25,34 @@
 # Output: 1
 
 from typing import List
+import heapq
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        return 0
+        n = len(stones)
+        
+        # Make max heap
+        for i in range(n):
+            stones[i] = -stones[i]
+            
+        heapq.heapify(stones)
+        
+        # Loop until there is one stone left
+        while len(stones) > 1:
+            
+            # Pop off two largest stones
+            s1 = -heapq.heappop(stones)
+            s2 = -heapq.heappop(stones)
+            
+            # If stones are different, add difference back to heap
+            if s1 != s2:
+                heapq.heappush(stones, -(s1 - s2))
+            # Otherwise the stones were the same, and they are both destroyed
+        
+        # Since we made max heap, return negative of last value
+        return -stones[0] if stones else 0
 
+stones = [2,7,4,1,8,1]
+# stones = [1]
+# stones = [2,2]
+print(Solution().lastStoneWeight(stones))
