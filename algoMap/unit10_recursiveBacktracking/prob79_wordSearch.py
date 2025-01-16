6f97938435c7cd854111076631179e771ec0f728
SuperSonicSeven <supersonicseven@gmail.com>
2025-01-16 13:33:41 -0600

    first solution to word search

--- algoMap/unit10_recursiveBacktracking/prob79_wordSearch.py
+++ algoMap/unit10_recursiveBacktracking/prob79_wordSearch.py
@@ -23,16 +23,50 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        res, sol = [], []
+        foundWord = [False]
+        w_len = len(word)
         
-        def backtrack():
-            if sol == word:
-                res.append(''.join(sol[:]))
+        m = len(board)
+        n = len(board[0])
+        
+        traveledSpaces = []
+        
+        def backtrack(i,j,p):
+            if board[i][j] != word[p]:
                 return
             
-        backtrack() 
-        return res
+            p += 1
+            if p == w_len:
+                foundWord[0] = True
+                return
+            
+            traveledSpaces.append([i, j])
+            
+            # Choosing direction to move
+            # RIGHT
+            if j < n-1 and [i, j+1] not in traveledSpaces:
+                backtrack(i, j+1, p)
+            
+            # DOWN    
+            if i < m-1 and [i+1, j] not in traveledSpaces:
+                backtrack(i+1, j, p)
+                
+            # LEFT
+            if j > 0 and [i, j-1] not in traveledSpaces:
+                backtrack(i, j-1, p)
+            
+            # UP
+            if i > 0 and [i-1, j] not in traveledSpaces:
+                backtrack(i-1, j, p)
+            
+            traveledSpaces.pop()              
+        
+        for i in range(m):
+            for j in range(n):
+                backtrack(i,j,0)
+                 
+        return foundWord[0]
     
 board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-word = "ABCCED"
+word = "GUY"
 print(Solution().exist(board, word))
