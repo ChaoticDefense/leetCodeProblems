8e2723994c87b8f38f919b3c3b02492127323f6c
ChaoticDefense <supersonicseven@gmail.com>
2025-01-18 21:09:08 -0600

    recursive dfs solution for number of islands

--- algoMap/unit11_graphs/prob200_numberOfIslands.py
+++ algoMap/unit11_graphs/prob200_numberOfIslands.py
@@ -31,35 +31,58 @@ class Solution:
         total = 0
         m, n = len(grid), len(grid[0])
         
-        # Iterative DFS
-        # Loop through grid until land is found
+        # # Iterative DFS
+        # # Loop through grid until land is found
+        # for i in range(m):
+        #     for j in range(n):
+        #         if grid[i][j] == "1":
+        #             # increase total by 1
+        #             total += 1
+                    
+        #             # Add current land to stack
+        #             stack = [(i,j)]
+                    
+        #             # Loop until no more in stack
+        #             while stack:
+        #                 # Get coords from stack
+        #                 a, b = stack.pop()
+                        
+        #                 # If current coords are out of bounds or isn't land, just continue
+        #                 if a >= m or a < 0 or b >= n or b < 0 or grid[a][b] != "1":
+        #                     continue
+                        
+        #                 # We're at land within bounds.
+        #                 # Mark it as sen by changing to water
+        #                 grid[a][b] = "0"
+                        
+        #                 # All adjacent squares to stack and check them
+        #                 stack.append((a+1,b))
+        #                 stack.append((a-1,b))
+        #                 stack.append((a,b+1))
+        #                 stack.append((a,b-1))
+                        
+        # Recursive DFS              
+        def dfs(i,j):
+            # Very similar to iterative dfs checks
+            if i >= m or i < 0 or j >= n or j < 0 or grid[i][j] != "1":
+                return
+            
+            grid[i][j] = "0"
+            
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+        
+        # Loop over whole grid            
         for i in range(m):
             for j in range(n):
+                # If current grid is land, add to total
                 if grid[i][j] == "1":
-                    # increase total by 1
                     total += 1
                     
-                    # Add current land to stack
-                    stack = [(i,j)]
-                    
-                    # Loop until no more in stack
-                    while stack:
-                        # Get coords from stack
-                        a, b = stack.pop()
-                        
-                        # If current coords are out of bounds or isn't land, just continue
-                        if a >= m or a < 0 or b >= n or b < 0 or grid[a][b] != "1":
-                            continue
-                        
-                        # We're at land within bounds.
-                        # Mark it as sen by changing to water
-                        grid[a][b] = "0"
-                        
-                        # All adjacent squares to stack and check them
-                        stack.append((a+1,b))
-                        stack.append((a-1,b))
-                        stack.append((a,b+1))
-                        stack.append((a,b-1))
+                    # Then flood fill current land with water to mark as seen
+                    dfs(i,j)
         
         return total
     
