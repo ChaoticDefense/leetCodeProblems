ee18a12c66fd4a97bf7c4d7f80fa76fc3c753108
ChaoticDefense <supersonicseven@gmail.com>
2025-01-23 20:56:04 -0600

    added BFS solutions to both course schedule problems

--- algoMap/unit11_graphs/prob207_courseSchedule.py
+++ algoMap/unit11_graphs/prob207_courseSchedule.py
@@ -21,7 +21,7 @@
 # To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
 
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
@@ -31,49 +31,87 @@ class Solution:
         for a, b in prerequisites:
             D[a].append(b)
     
-        # Constants for the state of each course
-        UNVISITED = 0
-        VISITING = 1
-        VISITED = 2
+        # # Constants for the state of each course
+        # UNVISITED = 0
+        # VISITING = 1
+        # VISITED = 2
 
-        # Make array of states for each course
-        states = [UNVISITED] * numCourses
+        # # Make array of states for each course
+        # states = [UNVISITED] * numCourses
         
-        # Recursive function
-        def dfs(node):
-            # Get state of current node
-            state = states[node]
+        # # Recursive function
+        # def dfs(node):
+        #     # Get state of current node
+        #     state = states[node]
             
-            # At a course we already visited and know we can finish
-            if state == VISITED:
-                return True
+        #     # At a course we already visited and know we can finish
+        #     if state == VISITED:
+        #         return True
             
-            # At a course we currently still visiting
-            # We found a loop in the courses, therefore cannot
-            # complete all courses
-            elif state == VISITING:
-                return False
+        #     # At a course we currently still visiting
+        #     # We found a loop in the courses, therefore cannot
+        #     # complete all courses
+        #     elif state == VISITING:
+        #         return False
             
-            # At a course that's unvisited, set state to visiting
-            states[node] = VISITING
+        #     # At a course that's unvisited, set state to visiting
+        #     states[node] = VISITING
             
-            # Check each neighbor if there's a loop
-            for nei_node in D[node]:
-                if not dfs(nei_node):
-                    return False
+        #     # Check each neighbor if there's a loop
+        #     for nei_node in D[node]:
+        #         if not dfs(nei_node):
+        #             return False
             
-            # No problems found from current node, set to visited and return True   
-            states[node] = VISITED
-            return True
+        #     # No problems found from current node, set to visited and return True   
+        #     states[node] = VISITED
+        #     return True
         
-        # Iterate over each node in graph 
+        # # Iterate over each node in graph 
+        # for i in range(numCourses):
+        #     # If there's a problem at any node, return False
+        #     if not dfs(i):
+        #         return False
+        
+        # # No problems found
+        # return True
+
+        # BFS Solution:
+        # Keeping track of how many nodes we visited
+        count = 0
+        
+        # Keep track of how many nodes point to the current node
+        # This is known as "in-degree"
+        indegrees = [0] * numCourses
         for i in range(numCourses):
-            # If there's a problem at any node, return False
-            if not dfs(i):
-                return False
+            for j in D[i]:
+                indegrees[j] += 1
         
-        # No problems found
-        return True
+        # Init a queue and append only the nodes with 0 indegrees
+        # AKA "sink nodes"        
+        q = deque([i for i in range(numCourses) if indegrees[i] == 0])
+
+        # Keep looping while queue is not empty
+        while q:
+            # Pop node from stack and add 1 to visited count
+            node = q.popleft()
+            count += 1
+            
+            # Loop through neighbors
+            for nei in D[node]:
+                # Decrement one from indegrees to keep track we are visiting
+                # current neighbor from current node
+                indegrees[nei] -= 1
+                
+                # If we have processed all indegrees of neighbor,
+                # add it to the queue because it is now essentially
+                # a "sink node"
+                if indegrees[nei] == 0:
+                    q.append(nei)
+        
+        # After processing the whole graph, check if the number of nodes
+        # we visited equal the number of nodes in graph
+        return count == numCourses
+
 
 # numCourses = 2
 # prerequisites = [[1,0]]
@@ -81,6 +119,9 @@ class Solution:
 # numCourses = 2
 # prerequisites = [[1,0],[0,1]]
 
-numCourses = 3
-prerequisites = [[0,1], [1,2]]
+# numCourses = 3
+# prerequisites = [[0,1], [1,2]]
+
+numCourses = 4
+prerequisites = [[1,0],[2,0],[3,1],[3,2]] 
 print(Solution().canFinish(numCourses, prerequisites))
--- algoMap/unit11_graphs/prob210_courseScheduleII.py
+++ algoMap/unit11_graphs/prob210_courseScheduleII.py
@@ -25,7 +25,7 @@
 # Output: [0]
 
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
@@ -35,59 +35,97 @@ class Solution:
         for u, v in prerequisites:
             D[u].append(v)
             
-        # The input of prerequisites [a_i, b_i] mean that b_i needs to be completed first
-        # I.e [0,1] would look like 1 -> 0
+        # # The input of prerequisites [a_i, b_i] mean that b_i needs to be completed first
+        # # I.e [0,1] would look like 1 -> 0
     
-        # Constants for the state of each course
-        UNVISITED = 0
-        VISITING = 1
-        VISITED = 2
+        # # Constants for the state of each course
+        # UNVISITED = 0
+        # VISITING = 1
+        # VISITED = 2
 
-        # Make array of states for each course
-        states = [UNVISITED] * numCourses
+        # # Make array of states for each course
+        # states = [UNVISITED] * numCourses
         
-        # Init output array
-        ans = []
+        # # Init output array
+        # ans = []
         
-        # DFS function on each node
-        def dfs(node):
-            # Get current state
-            state = states[node]
+        # # DFS function on each node
+        # def dfs(node):
+        #     # Get current state
+        #     state = states[node]
             
-            # If we already know we can take the course, just return
-            if state == VISITED:
-                return True
+        #     # If we already know we can take the course, just return
+        #     if state == VISITED:
+        #         return True
             
-            # We found a node we're currently visiting
-            # Therefore we found a loop
-            if state == VISITING:
-                return False
+        #     # We found a node we're currently visiting
+        #     # Therefore we found a loop
+        #     if state == VISITING:
+        #         return False
             
-            # Node is unvisited, set to visiting
-            states[node] = VISITING
+        #     # Node is unvisited, set to visiting
+        #     states[node] = VISITING
             
-            # Do DFS on each of the neighbor nodes
-            for nei in D[node]:
-                if not dfs(nei):
-                    return False
+        #     # Do DFS on each of the neighbor nodes
+        #     for nei in D[node]:
+        #         if not dfs(nei):
+        #             return False
             
-            # After searching all neighbors and didn't find a cycle,
-            # add current node to order list and mark as visited
-            ans.append(node)
-            states[node] = VISITED
-            return True
+        #     # After searching all neighbors and didn't find a cycle,
+        #     # add current node to order list and mark as visited
+        #     ans.append(node)
+        #     states[node] = VISITED
+        #     return True
         
-        # Do DFS on each node    
+        # # Do DFS on each node    
+        # for i in range(numCourses):
+        #     if not dfs(i):
+        #         # Found a cycle, can't do topological sort
+        #         return []
+        
+        # # In a NORMAL topological sort, we would return ans[::-1] because
+        # # we append the "last" node first, but since this problem has
+        # # essentially v -> u, instead of normally u -> v, we just return the
+        # # normal list  
+        # return ans
+    
+    
+        # BFS Solution:
+        # Keeping track of how many nodes we visited
+        order = []
+        
+        # Keep track of how many nodes point to the current node
+        # This is known as "in-degree"
+        indegrees = [0] * numCourses
         for i in range(numCourses):
-            if not dfs(i):
-                # Found a cycle, can't do topological sort
-                return []
+            for j in D[i]:
+                indegrees[j] += 1
         
-        # In a NORMAL topological sort, we would return ans[::-1] because
-        # we append the "last" node first, but since this problem has
-        # essentially v -> u, instead of normally u -> v, we just return the
-        # normal list  
-        return ans
+        # Init a queue and append only the nodes with 0 indegrees
+        # AKA "sink nodes"        
+        q = deque([i for i in range(numCourses) if indegrees[i] == 0])
+
+        # Keep looping while queue is not empty
+        while q:
+            # Pop node from stack and add node to order list
+            node = q.popleft()
+            order.append(node)
+            
+            # Loop through neighbors
+            for nei in D[node]:
+                # Decrement one from indegrees to keep track we are visiting
+                # current neighbor from current node
+                indegrees[nei] -= 1
+                
+                # If we have processed all indegrees of neighbor,
+                # add it to the queue because it is now essentially
+                # a "sink node"
+                if indegrees[nei] == 0:
+                    q.append(nei)
+        
+        # After processing the whole graph, check if the number of nodes
+        # we visited equal the number of nodes in graph
+        return order[::-1] if len(order) == numCourses else []
     
 numCourses = 4
 prerequisites = [[1,0],[2,0],[3,1],[3,2]] 
